0684e9b2cdc830b79517fb9b98971cd2e084f08b
fezzle <ericjame@gmail.com>
2014-10-09 23:05:46 -0400

    add ppm

---
+++ GccApplication1/ppm.c
@@ -0,0 +1,63 @@
+/*
+ * ppm.c
+ *
+ * Created: 9/27/2014 7:26:13 PM
+ *  Author: eric
+ */ 
+#include <inttypes.h>
+#include <avr/interrupt.h>
+#include <avr/io.h>
+
+#include "ppm.h"
+
+int16_t ch6;
+int16_t ch7;
+uint8_t currentchannel;
+
+/* 
+** High signal is marked from current point until Timer1 OVF
+** Low signal is from Timer1 OVF to Compare A.
+*/
+/*
+ISR(TIMER1_COMPA_vect) {
+	// set PPM_PIN for channel[currentchannel] time plus 500
+	int8_t val=0;
+	switch (currentchannel) {
+		case 6:	val = ch6;
+				PORTD = PORTD | _BV(CH6_PIN);
+				break;
+		case 7: val = ch7;
+				PORTD = PORTD | _BV(CH7_PIN);
+				break;
+		case 8: val = 5000;
+				break;
+	}
+	TCNT1 = -(1500 + val);		
+	PORTD = PORTD | _BV(PPM_PIN);
+}
+
+ISR(TIMER1_OVF_vect) {
+	// set PPM_PIN for channel[currentchannel] time plus 500
+	int8_t val=0;
+	switch (currentchannel) {
+		case 6:	PORTD = PORTD & ~_BV(CH6_PIN);
+				break;
+		case 7: PORTD = PORTD & ~_BV(CH7_PIN);
+				break;
+	}
+
+	PORTD = PORTD | _BV(PPM_PIN);
+
+	currentchannel++;
+	if (currentchannel == 9) {
+		currentchannel = 1;
+	}
+}
+*/
+
+void init_ppm() {
+	TCCR1B = _BV(CS11);
+	OCR1A = 200;
+	TIMSK = TIMSK | _BV(TOIE1) | _BV(OCIE1A);
+	DDRD = DDRD | _BV(PPM_PIN) | _BV(CH5_PIN) | _BV(CH6_PIN) | _BV(CH7_PIN) | _BV(CH8_PIN);
+}
---
+++ GccApplication1/ppm.h
@@ -0,0 +1,18 @@
+/*
+ * ppm.h
+ *
+ * Created: 9/27/2014 7:25:55 PM
+ *  Author: eric
+ */ 
+
+
+#ifndef PPM_H_
+#define PPM_H_
+
+#include <stdio.h>
+
+#include "main.h"
+
+void init_ppm();
+
+#endif /* PPM_H_ */
